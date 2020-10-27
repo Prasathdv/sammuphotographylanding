df0cf73b697a8f8febdf5abd641eaf65add733a3
Prasath D.V <prasath.dv@gmail.com>
2020-10-27 18:56:44 +0530

    Initial Commit

---
+++ Assets/script/main.js
@@ -0,0 +1,11 @@
+// Animate smooth scrolling using jquery
+
+$('#view-work').on('click', function(){
+    const images = $('#images').position().top;
+
+    $('html, body').animate(
+        {
+            scrollTop: images
+        },900
+    )
+})
